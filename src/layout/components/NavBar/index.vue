ab388bcde9e1950b0f6ae44c8cc510f6ef2ceda9
wuzhiyong <18735127552@163.com>
2020-02-06 19:00:35 +0800

    增加相关代码

--- src/layout/components/NavBar/index.vue
+++ src/layout/components/NavBar/index.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="navbar">
+    <hamburger id="hamburger-container" :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" />
+
     <router-link to="/home">Home</router-link> |
     <router-link to="/about">About</router-link>  
   </div>
 </template>
 
 <script>
+  import Hamburger from '@/components/Hamburger'
+
   export default {
-    name: 'NavBar'
+    name: 'NavBar',
+    components: {
+      Hamburger
+    },
+    data() {
+      return {
+        sidebar: {
+          opened: false
+        }
+      }
+    },
+    methods: {
+      toggleSideBar() {
+        this.sidebar.opened = !this.sidebar.opened
+        console.log('side:', this.sidebar.opened)
+      }
+    },
   }
 </script>
 
@@ -26,6 +46,16 @@
 
       &.router-link-exact-active {
         color: #42b983;
+      }
+    }
+
+    .hamburger-container {
+      float: left;
+      cursor: pointer;
+      transition: background .3s;
+      -webkit-tap-highlight-color:transparent;
+      &:hover {
+        background: rgba(0, 0, 0, .025)
       }
     }
   }
--- src/layout/components/SideBar/index.vue
+++ src/layout/components/SideBar/index.vue
@@ -3,34 +3,16 @@
     <!-- logo -->
 
     <!-- menu -->
-    <el-scrollbar wrap-class="scrollbar-wrapper">
+    <el-scrollbar wrap-class="scrollbar-wrapper" style="height: 100%">
       <el-menu
         :default-active="activeMenu"
-        :collapse="false"
+        :collapse="isCollapse"
         :background-color="variables.menuBg"
         :text-color="variables.menuText"
         :unique-opened="false"
         :active-text-color="variables.menuActiveText"
         :collapse-transition="false"
         mode="vertical">
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>导航一</span>
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
         <el-menu-item index="2">
           <i class="el-icon-menu"></i>
           <span slot="title">导航二</span>
@@ -56,12 +38,15 @@
     name: 'SideBar',
     data() {
       return {
-        activeMenu: '1-1'
+        activeMenu: '2'
       }
     },
     computed: {
       variables() {
         return variables
+      },
+      isCollapse() {
+        return false
       }
     }
   }
@@ -85,7 +70,6 @@
 
 <style lang="scss">
   .scrollbar-wrapper {
-    overflow-x: hidden !important;
-    height: 100% !important
+    overflow-x: hidden !important
   }
 </style>
