a92c5238595e6781e844e132285139a7f43c1e84
wuzhiyong <18735127552@163.com>
2020-02-22 09:59:03 +0800

    节流方法的引入和使用

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,13 +4,13 @@
  * @param {Function} func
  * @param {Number} wait
  * @param {Boolean} isImmediate
+ * @description 当调用动作过n毫秒后，才会执行该动作，若在这n毫秒内又调用此动作则将重新计算执行时间
  * @returns
  */
 export function _debounce(func, wait, isImmediate) {
   var timeout
   return function () {
-    var _this = this,
-        _arguments = arguments
+    var _this = this, _arguments = arguments
     // 触发就清除定时器
     clearTimeout(timeout)
     // 立即执行
@@ -30,19 +30,39 @@ export function _debounce(func, wait, isImmediate) {
 
 
 /**
+ * @name 节流
+ * @description 函数节流就是预定一个函数只有在大于等于执行周期时才执行，周期内调用不执行
+ * @export
+ * @param {*} func
+ * @param {*} wait
+ * @returns
+ */
+export function _throttle(func, wait) {
+  var last = 0
+  return function () {
+    var now = +new Date()
+    if (now - last > wait) {
+      func.apply(this, arguments)
+      last = now
+    }
+  }
+}
+
+
+/**
  * 获取当前用户的IP
- * @description 会有浏览器的要求
+ * @description 会有浏览器的要求,callback的使用，谷歌浏览器需开启设置
  * @export
  * @param {*} callback
  */
 export function _getUserIP(callback) {
   var ip = ''
   window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for Firefox and chrome
-  var pc = new RTCPeerConnection({ iceServers: [] }), noop = function () { };
+  var pc = new RTCPeerConnection({ iceServers: [] }), noop = function(){ };
   pc.createDataChannel('') //create a bogus data channel
   pc.createOffer(pc.setLocalDescription.bind(pc), noop) // create offer and set local description
   pc.onicecandidate = function (ice) {
-  if (ice && ice.candidate && ice.candidate.candidate) {
+    if (ice && ice.candidate && ice.candidate.candidate) {
       ip = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g.exec(ice.candidate.candidate)[1]
       pc.onicecandidate = noop
       callback(ip)
--- src/views/test/test0216.vue
+++ src/views/test/test0216.vue
@@ -2,44 +2,65 @@
   <div class="test-page pl-5">
     <h1>This is an test0216 page</h1>
     <p>学习使用防抖节流知识</p>
-    <section class="test-section" id="mouseOver" @mouseover.passive="_mouseOver1"></section>
+    <el-input v-model="input" placeholder="请输入内容" @keyup.native="_input"></el-input><span>防抖结果:{{ inputResult }}</span>
+    <section class="test-section" id="mouseOver" @mouseover="_mouseOver"></section>
+    <el-button @click="_btnClick">多次点击节流</el-button><span>多次点击的次数:{{ btnCount }}</span>
     <p>当前用户的IP：{{userIP}}</p>
   </div>
 </template>
 
 <script>
-  import { _debounce ,  _getUserIP} from '@/utils'
+  import { _debounce, _throttle,  _getUserIP} from '@/utils'
 
   export default {
     data() {
       return {
         count: 0,
-        userIP: ''
+        btnCount: 0,
+        userIP: '',
+        input: '',
+        inputResult: ''
       }
     },
     mounted() {
-      let that = this
-      _getUserIP(function(ip) {
-        console.log(ip)
-        that.userIP = ip
-      })
+      this._getIp()
     },
     methods: {
-      _mouseOver() {
-        var mouseOver = document.getElementById('mouseOver')
-        this.count ++
-        mouseOver.innerHTML = this.count
+      /** 
+       * 获取用户IP
+      */
+      _getIp() {
+        let that = this
+        _getUserIP(function(ip) {
+          console.log(ip)
+          that.userIP = ip
+        })
       },
 
-      _mouseOver1: _debounce (function() {
-        // this.mouseOver()
+      /** 
+       * 鼠标滑过事件，使用防抖函数
+      */
+      _mouseOver: _debounce(function() {
         console.log('触发了')
         var mouseOver = document.getElementById('mouseOver')
         this.count ++
-        mouseOver.innerHTML = this.count
+        mouseOver.innerHTML = `防抖效果：${this.count}`
       }, 1000, true),
 
+      /** 
+       * input的输入事件
+      */
+      _input: _debounce(function() {
+        console.log('input:', this.input)
+        this.inputResult = this.input
+      }, 1000, true),
 
+     /** 
+      * 高频率点击节流处理
+     */
+      _btnClick: _throttle(function() {
+        this.btnCount ++
+      }, 2000)
     },
   }
 </script>
@@ -48,8 +69,21 @@
   .test-page {
     .test-section {
       height: 200px;
-      margin-right: 5px;
-      border: 1px solid;
+      border: 1px solid
+    }
+
+    .el-input {
+      width: 200px;
+      margin-right: 10px
+    }
+
+    .el-button {
+      border-color: aquamarine
+    }
+
+    .test-section, .el-button {
+      margin-top: 15px;
+      margin-right: 5px
     }
   }
 </style>
